42d4c97484094991fd58a8962ffd39c23fc0dd65
Kayke Sampaio Freitas <kaykesampaio196@gmail.com>
2022-05-30 21:05:31 -0300

    criei o teste

---
+++ teste.py
@@ -0,0 +1,7 @@
+# progama não sei se funciona
+    # talveis não funcione no VSC ou em algum outro lugar mas sei que funciona no GDB online
+
+nome = str(input('Digíte seu nome aqui: ')).strip
+
+print('\nOlá, {} prazer em te conhecer! '.format(nome))
+
